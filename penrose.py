0f81f77563f0ca3d0eeebc5c11466a2289d261ec
Andrew Mah <andrewmah@MacBook-Pro.local>
2020-10-17 21:17:30 -0400

    added key bindings to change deflation amount

--- penrose.py
+++ penrose.py
@@ -107,9 +107,19 @@ class Penrose:
         self.width = width
         self.height = height
         self.origin = np.array([width/2, height/2])
-        self.radius = np.sqrt(width**2 + height**2)
+        #self.radius = np.sqrt(width**2 + height**2)
+        self.radius = width/2
 
-        #initial seed shape "sun"
+
+
+        #initial seed shape "sun" with 5 deflations
+        self.initialize_sun()
+        self.deflation_count = 3
+        for i in range(self.deflation_count):
+            self.deflate()
+
+
+    def initialize_sun(self):
         self.shapes = []
         for i in range(5):
             theta1 = 2*np.pi/5 * i
@@ -120,24 +130,24 @@ class Penrose:
             self.shapes.append(acTriangle(np.array([0, 0]),
                                           np.array([self.radius*np.cos(theta1+2*theta2), self.radius*np.sin(theta1+2*theta2)]),
                                           np.array([self.radius*np.cos(theta1+theta2), self.radius*np.sin(theta1+theta2)])))
-        view = np.array([width/2, height/2])
+        view = np.array([self.width/2, self.height/2])
         for shape in self.shapes:
             shape.a = shape.a + view
             shape.b = shape.b + view
             shape.c = shape.c + view
 
-        #fill interior of seed shape
-        self.shapes = self.deflate()
+
+
 
     def deflate(self):
-        prev_list = self.shapes
-        for i in range(4): #TODO how many
-            new_list = []
-            for shape in prev_list:
-                for new_shape in shape.deflate():
-                    new_list.append(new_shape)
-            prev_list = new_list
-        return prev_list
+        new_list = []
+        for shape in self.shapes:
+            for new_shape in shape.deflate():
+                new_list.append(new_shape)
+        self.shapes = new_list
+
+
+
 
     def render(self):
         for tri in self.shapes:
@@ -148,8 +158,25 @@ class Penrose:
             tri.render_arcs(self.image) #arcs
 
 
+def deflate_event(event):
+    pattern.deflation_count += 1
+    pattern.deflate()
+    pattern.render()
+
+def inflate_event(event):
+    if pattern.deflation_count <= 0:
+        return
+    pattern.deflation_count -= 1
+    pattern.initialize_sun()
+    for i in range(pattern.deflation_count):
+        pattern.deflate()
+    pattern.render()
 
 if __name__ == '__main__':
     pattern = Penrose()
     pattern.render()
+
+    pattern.window.bind('-', inflate_event)
+    pattern.window.bind('=', deflate_event)
+
     pattern.window.mainloop()
